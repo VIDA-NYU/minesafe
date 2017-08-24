acc96c12d34f7ba496bcac444d9dcbc316e1593c
Carlos Bautista <cbi210@nyu.edu>
2017-08-24 17:02:58 -0400

    implements splash screen

---
+++ MINE_SAFE/app/src/main/java/com/example/android/minesafe/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.example.android.minesafe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by carlosbautista on 8/24/17.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
